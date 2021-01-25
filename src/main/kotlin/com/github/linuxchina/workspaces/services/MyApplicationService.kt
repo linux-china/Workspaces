3edd401c7e7b512e8c8122fee0b213e5790ef8d9
GitHub Action <action@github.com>
2021-01-25 23:51:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/linuxchina/workspaces/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linuxchina/workspaces/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linuxchina.workspaces.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linuxchina.workspaces.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/linuxchina/workspaces/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linuxchina/workspaces/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.linuxchina.workspaces.services
 
+import com.github.linuxchina.workspaces.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
