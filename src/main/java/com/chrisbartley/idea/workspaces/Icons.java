278b017f81b2d47efd314b55b8c9fdbc0a233648
Libing Chen <libing.chen@gmail.com>
2023-05-17 14:41:06 +0800

    chore: polishing

--- src/main/java/com/chrisbartley/idea/workspaces/Icons.java
+++ src/main/java/com/chrisbartley/idea/workspaces/Icons.java
@@ -1,49 +1,42 @@
-/*    */ package com.chrisbartley.idea.workspaces;
-/*    */ 
-/*    */ import javax.swing.Icon;
-/*    */ import javax.swing.ImageIcon;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public final class Icons
-/*    */ {
-/* 11 */   public static final Icon WORKSPACES = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/workspace.png"));
-/*    */   
-/* 13 */   public static final Icon INCLUDED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/check.png"));
-/* 14 */   public static final Icon EXCLUDED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/noCheck.png"));
-/*    */   
-/* 16 */   public static final Icon OPENED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/check.png"));
-/* 17 */   public static final Icon PARTIALLY_OPENED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/greyCheck.png"));
-/* 18 */   public static final Icon UNOPENED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/noCheck.png"));
-/*    */   
-/* 20 */   public static final Icon ACTIVE_WORKSPACE = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/activeWorkspace.png"));
-/* 21 */   public static final Icon INACTIVE_WORKSPACE = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/inactiveWorkspace.png"));
-/*    */   
-/* 23 */   public static final Icon ADD_WORKSPACE = new ImageIcon(Icons.class.getResource("/general/add.png"));
-/* 24 */   public static final Icon REMOVE_WORKSPACE = new ImageIcon(Icons.class.getResource("/general/remove.png"));
-/*    */   
-/* 26 */   public static final Icon CONFIGURE_WORKSPACE = new ImageIcon(Icons.class.getResource("/actions/properties.png"));
-/*    */   
-/* 28 */   public static final Icon CLOSE_WORKSPACE = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/workspaceClosed.png"));
-/* 29 */   public static final Icon OPEN_WORKSPACE = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/workspaceOpen.png"));
-/*    */   
-/* 31 */   public static final Icon INCLUDE_WORKSPACE_ITEM = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/include.png"));
-/* 32 */   public static final Icon EXCLUDE_WORKSPACE_ITEM = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/exclude.png"));
-/*    */   
-/* 34 */   public static final Icon EXCLUDE_WORKSPACED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/excludeWorkspaced.png"));
-/*    */   
-/* 36 */   public static final Icon WORKSPACES_CONFIGURABLE = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/workspacesConfigurable.png"));
-/*    */   
-/* 38 */   public static final Icon PINNED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/pinned.png"));
-/* 39 */   public static final Icon UNPINNED = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/unpinned.png"));
-/*    */   
-/* 41 */   public static final Icon PINNED_AND_OPEN = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/pinnedAndOpen.png"));
-/* 42 */   public static final Icon PINNED_AND_PARTIALLY_OPEN = new ImageIcon(Icons.class.getResource("/com/chrisbartley/idea/workspaces/icons/pinnedAndPartiallyOpen.png"));
-/*    */ }
+package com.chrisbartley.idea.workspaces;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
 
 
-/* Location:              /Users/linux_china/Downloads/Workspaces_293.jar!/com/chrisbartley/idea/workspaces/Icons.class
- * Java compiler version: 2 (46.0)
- * JD-Core Version:       1.1.3
- */
+public final class Icons {
+    public static final Icon WORKSPACES = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/workspace.png", Icons.class);
+
+    public static final Icon INCLUDED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/check.png", Icons.class);
+    public static final Icon EXCLUDED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/noCheck.png", Icons.class);
+
+    public static final Icon OPENED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/check.png", Icons.class);
+    public static final Icon PARTIALLY_OPENED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/greyCheck.png", Icons.class);
+    public static final Icon UNOPENED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/noCheck.png", Icons.class);
+
+    public static final Icon ACTIVE_WORKSPACE = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/activeWorkspace.png", Icons.class);
+    public static final Icon INACTIVE_WORKSPACE = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/inactiveWorkspace.png", Icons.class);
+
+    public static final Icon ADD_WORKSPACE = IconLoader.getIcon("/general/add.png", Icons.class);
+    public static final Icon REMOVE_WORKSPACE = IconLoader.getIcon("/general/remove.png", Icons.class);
+
+    public static final Icon CONFIGURE_WORKSPACE = IconLoader.getIcon("/actions/properties.png", Icons.class);
+
+    public static final Icon CLOSE_WORKSPACE = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/workspaceClosed.png", Icons.class);
+    public static final Icon OPEN_WORKSPACE = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/workspaceOpen.png", Icons.class);
+
+    public static final Icon INCLUDE_WORKSPACE_ITEM = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/include.png", Icons.class);
+    public static final Icon EXCLUDE_WORKSPACE_ITEM = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/exclude.png", Icons.class);
+
+    public static final Icon EXCLUDE_WORKSPACED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/excludeWorkspaced.png", Icons.class);
+
+    public static final Icon WORKSPACES_CONFIGURABLE = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/workspacesConfigurable.png", Icons.class);
+
+    public static final Icon PINNED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/pinned.png", Icons.class);
+    public static final Icon UNPINNED = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/unpinned.png", Icons.class);
+
+    public static final Icon PINNED_AND_OPEN = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/pinnedAndOpen.png", Icons.class);
+    public static final Icon PINNED_AND_PARTIALLY_OPEN = IconLoader.getIcon("/com/chrisbartley/idea/workspaces/icons/pinnedAndPartiallyOpen.png", Icons.class);
+}
+
