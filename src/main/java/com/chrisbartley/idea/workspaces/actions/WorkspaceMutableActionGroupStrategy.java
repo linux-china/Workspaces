e5a93ad15e53d827d741d730f770a9923aff3139
Libing Chen <libing.chen@gmail.com>
2023-05-17 14:42:38 +0800

    chore: polishing

--- src/main/java/com/chrisbartley/idea/workspaces/actions/WorkspaceMutableActionGroupStrategy.java
+++ src/main/java/com/chrisbartley/idea/workspaces/actions/WorkspaceMutableActionGroupStrategy.java
@@ -1,23 +1,12 @@
-/*    */ package com.chrisbartley.idea.workspaces.actions;
-/*    */ 
-/*    */ import com.chrisbartley.idea.actions.MutableActionGroupStrategy;
-/*    */ import com.chrisbartley.idea.workspaces.WorkspaceManager;
-/*    */ import com.intellij.openapi.actionSystem.AnActionEvent;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public abstract class WorkspaceMutableActionGroupStrategy
-/*    */   extends MutableActionGroupStrategy
-/*    */ {
-/*    */   protected final WorkspaceManager getWorkspaceManager(AnActionEvent event) {
-/* 15 */     return (WorkspaceManager)getProject(event).getComponent(WorkspaceManager.class);
-/*    */   }
-/*    */ }
+package com.chrisbartley.idea.workspaces.actions;
 
+import com.chrisbartley.idea.actions.MutableActionGroupStrategy;
+import com.chrisbartley.idea.workspaces.WorkspaceManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 
-/* Location:              /Users/linux_china/Downloads/Workspaces_293.jar!/com/chrisbartley/idea/workspaces/actions/WorkspaceMutableActionGroupStrategy.class
- * Java compiler version: 2 (46.0)
- * JD-Core Version:       1.1.3
- */
+public abstract class WorkspaceMutableActionGroupStrategy extends MutableActionGroupStrategy {
+    protected final WorkspaceManager getWorkspaceManager(AnActionEvent event) {
+        return getProject(event).getService(WorkspaceManager.class);
+    }
+}
+
