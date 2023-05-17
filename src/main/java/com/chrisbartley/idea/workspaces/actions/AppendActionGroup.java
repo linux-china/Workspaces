f4b17aa35a08fee5c840e25d705001fe4768ce2f
Libing Chen <libing.chen@gmail.com>
2023-05-17 17:46:36 +0800

    app: AppendActionGroup

---
+++ src/main/java/com/chrisbartley/idea/workspaces/actions/AppendActionGroup.java
@@ -0,0 +1,20 @@
+package com.chrisbartley.idea.workspaces.actions;
+
+import com.chrisbartley.idea.workspaces.Icons;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import org.jetbrains.annotations.NotNull;
+
+
+public class AppendActionGroup extends DefaultActionGroup {
+    public AppendActionGroup() {
+        super("Append", true);
+    }
+
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        e.getPresentation().setIcon(Icons.INCLUDE_WORKSPACE_ITEM);
+    }
+
+}
