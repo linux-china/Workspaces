26d0fe14d02226e289bb8b5470f39c97aea729b2
Libing Chen <libing.chen@gmail.com>
2023-05-17 15:09:51 +0800

    chore: polishing

--- src/main/java/com/chrisbartley/idea/workspaces/WorkspaceState.java
+++ src/main/java/com/chrisbartley/idea/workspaces/WorkspaceState.java
@@ -1,114 +1,87 @@
-/*     */ package com.chrisbartley.idea.workspaces;
-/*     */ 
-/*     */ import java.util.Set;
-/*     */ import javax.swing.Icon;
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ public final class WorkspaceState
-/*     */ {
-/*     */   private final Set openUrls;
-/*     */   private final int numOpenFiles;
-/*     */   private final int numFiles;
-/*     */   private final boolean isPinned;
-/*     */   
-/*     */   public WorkspaceState(Set openUrls, int numFiles, boolean isPinned) {
-/*  18 */     this.openUrls = openUrls;
-/*  19 */     this.isPinned = isPinned;
-/*  20 */     this.numOpenFiles = openUrls.size();
-/*  21 */     this.numFiles = numFiles;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public Set getOpenUrls() {
-/*  26 */     return this.openUrls;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public boolean isOpen() {
-/*  31 */     return (this.numOpenFiles == this.numFiles);
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public boolean isPartiallyOpen() {
-/*  36 */     return (this.numOpenFiles > 0 && this.numOpenFiles < this.numFiles);
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public boolean isClosed() {
-/*  41 */     return (this.numOpenFiles == 0);
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public Icon getOpenedStatusIcon() {
-/*  46 */     if (this.numOpenFiles > 0)
-/*     */     {
-/*  48 */       return (this.numOpenFiles == this.numFiles) ? Icons.OPENED : Icons.PARTIALLY_OPENED;
-/*     */     }
-/*     */ 
-/*     */     
-/*  52 */     return Icons.UNOPENED;
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public int getNumOpenFiles() {
-/*  58 */     return this.numOpenFiles;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public int getNumFiles() {
-/*  63 */     return this.numFiles;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public boolean isPinned() {
-/*  68 */     return this.isPinned;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public Icon getPinnedStatusIcon() {
-/*  73 */     return this.isPinned ? Icons.PINNED : Icons.UNPINNED;
-/*     */   }
-/*     */ 
-/*     */   
-/*     */   public Icon getComboStatusIcon() {
-/*  78 */     if (this.isPinned) {
-/*     */       
-/*  80 */       if (this.numOpenFiles > 0)
-/*     */       {
-/*  82 */         return (this.numOpenFiles == this.numFiles) ? Icons.PINNED_AND_OPEN : Icons.PINNED_AND_PARTIALLY_OPEN;
-/*     */       }
-/*     */ 
-/*     */       
-/*  86 */       return Icons.PINNED;
-/*     */     } 
-/*     */ 
-/*     */ 
-/*     */     
-/*  91 */     return getOpenedStatusIcon();
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public String toString() {
-/*  97 */     StringBuffer str = new StringBuffer();
-/*     */     
-/*  99 */     str.append("isOpen()          = " + isOpen()).append("\n");
-/* 100 */     str.append("isPartiallyOpen() = " + isPartiallyOpen()).append("\n");
-/* 101 */     str.append("isClosed()        = " + isClosed()).append("\n");
-/* 102 */     str.append("isPinned()        = " + isPinned()).append("\n");
-/* 103 */     str.append("num files         = " + getNumFiles()).append("\n");
-/* 104 */     str.append("num open files    = " + getNumOpenFiles());
-/*     */     
-/* 106 */     return str.toString();
-/*     */   }
-/*     */ }
+package com.chrisbartley.idea.workspaces;
+
+import javax.swing.*;
+import java.util.Set;
 
 
-/* Location:              /Users/linux_china/Downloads/Workspaces_293.jar!/com/chrisbartley/idea/workspaces/WorkspaceState.class
- * Java compiler version: 2 (46.0)
- * JD-Core Version:       1.1.3
- */
+public final class WorkspaceState {
+    private final Set<String> openUrls;
+    private final int numOpenFiles;
+    private final int numFiles;
+    private final boolean isPinned;
+
+    public WorkspaceState(Set<String> openUrls, int numFiles, boolean isPinned) {
+        this.openUrls = openUrls;
+        this.isPinned = isPinned;
+        this.numOpenFiles = openUrls.size();
+        this.numFiles = numFiles;
+    }
+
+
+    public Set<String> getOpenUrls() {
+        return this.openUrls;
+    }
+
+
+    public boolean isOpen() {
+        return (this.numOpenFiles == this.numFiles);
+    }
+
+
+    public boolean isPartiallyOpen() {
+        return (this.numOpenFiles > 0 && this.numOpenFiles < this.numFiles);
+    }
+
+
+    public boolean isClosed() {
+        return (this.numOpenFiles == 0);
+    }
+
+
+    public Icon getOpenedStatusIcon() {
+        if (this.numOpenFiles > 0) {
+            return (this.numOpenFiles == this.numFiles) ? Icons.OPENED : Icons.PARTIALLY_OPENED;
+        }
+
+
+        return Icons.UNOPENED;
+    }
+
+
+    public int getNumOpenFiles() {
+        return this.numOpenFiles;
+    }
+
+
+    public int getNumFiles() {
+        return this.numFiles;
+    }
+
+
+    public boolean isPinned() {
+        return this.isPinned;
+    }
+
+
+    public Icon getPinnedStatusIcon() {
+        return this.isPinned ? Icons.PINNED : Icons.UNPINNED;
+    }
+
+
+    public Icon getComboStatusIcon() {
+        if (this.isPinned) {
+
+            if (this.numOpenFiles > 0) {
+                return (this.numOpenFiles == this.numFiles) ? Icons.PINNED_AND_OPEN : Icons.PINNED_AND_PARTIALLY_OPEN;
+            }
+
+
+            return Icons.PINNED;
+        }
+
+
+        return getOpenedStatusIcon();
+    }
+
+}
+
