4773318d37a19816408354ba89fa3f1ac1134203
Libing Chen <libing.chen@gmail.com>
2023-05-17 19:32:22 +0800

    app: CloseAllTabsAndOpenWorkspaceAction

---
+++ src/main/java/com/chrisbartley/idea/workspaces/actions/CloseAllTabsAndOpenWorkspaceAction.java
@@ -0,0 +1,36 @@
+package com.chrisbartley.idea.workspaces.actions;
+
+import com.chrisbartley.idea.util.RefreshableListModel;
+import com.chrisbartley.idea.workspaces.Workspace;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public final class CloseAllTabsAndOpenWorkspaceAction extends BaseWorkspaceAction {
+    private final JList<Workspace> jList;
+
+    public CloseAllTabsAndOpenWorkspaceAction(JList<Workspace> jList) {
+        super("Close tabs and open", "Close all tabs and open workspace", null);
+        this.jList = jList;
+    }
+
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = getProject(event);
+        if (project != null) {
+            if (!this.jList.getSelectionModel().isSelectionEmpty()) {
+                final FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+                for (VirtualFile openFile : fileEditorManager.getOpenFiles()) {
+                    fileEditorManager.closeFile(openFile);
+                }
+                getWorkspaceManager(project).openWorkspaces(this.jList.getSelectedValuesList());
+                RefreshableListModel<Workspace> model = (RefreshableListModel<Workspace>) this.jList.getModel();
+                model.refresh(this.jList.getSelectedIndices());
+            }
+        }
+    }
+}
+
