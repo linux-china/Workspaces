7db67c99c4f7581e4eede59ba2cefa77377e1f6c
Libing Chen <libing.chen@gmail.com>
2023-05-17 14:52:55 +0800

    chore: polishing

--- src/main/java/com/chrisbartley/idea/workspaces/actions/OpenWorkspaceAction.java
+++ src/main/java/com/chrisbartley/idea/workspaces/actions/OpenWorkspaceAction.java
@@ -1,53 +1,35 @@
-/*    */ package com.chrisbartley.idea.workspaces.actions;
-/*    */ 
-/*    */ import com.chrisbartley.idea.util.RefreshableListModel;
-/*    */ import com.chrisbartley.idea.workspaces.Icons;
-/*    */ import com.intellij.openapi.actionSystem.AnActionEvent;
-/*    */ import com.intellij.openapi.project.Project;
-/*    */ import java.util.Arrays;
-/*    */ import javax.swing.JList;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public final class OpenWorkspaceAction
-/*    */   extends BaseWorkspaceAction
-/*    */ {
-/*    */   private final JList list;
-/*    */   
-/*    */   public OpenWorkspaceAction(JList list) {
-/* 23 */     super("Open", "Open the selected workspace(s)", Icons.OPEN_WORKSPACE);
-/* 24 */     this.list = list;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public void actionPerformed(AnActionEvent event) {
-/* 29 */     Project project = getProject(event);
-/* 30 */     if (project != null)
-/*    */     {
-/* 32 */       if (!this.list.getSelectionModel().isSelectionEmpty()) {
-/*    */         
-/* 34 */         getWorkspaceManager(project).openWorkspaces(Arrays.asList(this.list.getSelectedValues()));
-/*    */ 
-/*    */         
-/* 37 */         RefreshableListModel model = (RefreshableListModel)this.list.getModel();
-/* 38 */         model.refresh(this.list.getSelectedIndices());
-/*    */       } 
-/*    */     }
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public void update(AnActionEvent event) {
-/* 45 */     event.getPresentation().setEnabled(!this.list.getSelectionModel().isSelectionEmpty());
-/*    */   }
-/*    */ }
+package com.chrisbartley.idea.workspaces.actions;
+
+import com.chrisbartley.idea.util.RefreshableListModel;
+import com.chrisbartley.idea.workspaces.Icons;
+import com.chrisbartley.idea.workspaces.Workspace;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public final class OpenWorkspaceAction extends BaseWorkspaceAction {
+    private final JList<Workspace> jList;
+
+    public OpenWorkspaceAction(JList<Workspace> jList) {
+        super("Open", "Open the selected workspace(s)", Icons.OPEN_WORKSPACE);
+        this.jList = jList;
+    }
+
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = getProject(event);
+        if (project != null) {
+            if (!this.jList.getSelectionModel().isSelectionEmpty()) {
+                getWorkspaceManager(project).openWorkspaces(this.jList.getSelectedValuesList());
+                RefreshableListModel model = (RefreshableListModel) this.jList.getModel();
+                model.refresh(this.jList.getSelectedIndices());
+            }
+        }
+    }
 
 
-/* Location:              /Users/linux_china/Downloads/Workspaces_293.jar!/com/chrisbartley/idea/workspaces/actions/OpenWorkspaceAction.class
- * Java compiler version: 2 (46.0)
- * JD-Core Version:       1.1.3
- */
+    public void update(AnActionEvent event) {
+        event.getPresentation().setEnabled(!this.jList.getSelectionModel().isSelectionEmpty());
+    }
+}
